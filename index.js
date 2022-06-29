8474b66fa207766ad7158377159568c257bb0b55
Caroline <alyseflorez@hotmail.fr>
2022-06-29 16:58:22 +0200

    exo boucles spécifiques

---
+++ index.js
@@ -0,0 +1,89 @@
+// 01
+
+// let array = [1, 2, 3, 4, 5]
+
+// let double = array.map((array) => { 
+//     return array * 2
+// })
+// console.log(double)
+
+
+// 02
+
+// let longNames = [
+// 	{
+// 		firstName: "Jane",
+// 		lastName: "Doe"
+// 	},
+// 	{
+// 		firstName: "John",
+// 		lastName: "Smith"
+// 	}
+// ]
+
+// let shortNames = longNames.map((longName) => { 
+//     return {
+//         name: `${longName.firstName} ${longName.lastName}`
+//     }
+// })
+//  console.log(shortNames)
+
+
+// 03
+
+// let elements = [1, "toto", 34, "javascript", 8]
+
+// let numbers = elements.filter((element) => { 
+//     return typeof element === "number"
+// })
+// console.log(numbers)
+
+// 04 
+
+// const numbers = [ 1, 2, 3, 4, 5, 6, 7, 8]
+
+// const even = numbers.filter((number) { 
+//     return number % 2 === 0
+// })
+// console.log(even)
+
+// 05
+// const cakes = [
+// 	{
+// 		name: "cake",
+// 		flavor: "vanilla",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "brownie",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "pie",
+// 		flavor: "strawberry",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "muffin",
+// 		flavor: "pistachio",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "donut",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// ]
+
+// const chocolates = cakes.filter((cake) => {
+//   return cake.flavor === "chocolate"
+// })
+// console.log(chocolates)
+
+// 06
+// const pie = cakes.find((cake) => {
+//   return cake.name === "pie"
+// })
+
+// console.log(pie)
